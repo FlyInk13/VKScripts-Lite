e050008478dc91ce3fc766e18f8cb073cbb9e863
Evgeniy Kotlyarov <dev@ifx.su>
2019-06-20 16:09:46 +0300

    public /build

---
+++ build/precache-manifest.b9b6628f69430962e497c573eb802a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ca35714098e97dbff92a",
+    "url": "./static/js/main.6cdb924a.chunk.js"
+  },
+  {
+    "revision": "4e2891e691e4f69e749e",
+    "url": "./static/js/2.373c0ce2.chunk.js"
+  },
+  {
+    "revision": "ca35714098e97dbff92a",
+    "url": "./static/css/main.3d458bee.chunk.css"
+  },
+  {
+    "revision": "4e2891e691e4f69e749e",
+    "url": "./static/css/2.7aa69d71.chunk.css"
+  },
+  {
+    "revision": "c3b006195c1d8faeb5a0dc76414093e3",
+    "url": "./index.html"
+  }
+];
